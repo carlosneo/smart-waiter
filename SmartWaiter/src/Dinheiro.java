f2f8f2079cd26bd609a1416b42aa09dba5abb315
itallorossi.lucas@gmail.com <itallorossi.lucas@gmail.com@8172dd28-21f5-c58d-ca4c-9ce7260f8b8e>
2011-12-06 23:29:49 +0000

    Implementação da Fabrica no Pagamento.

--- SmartWaiter/src/Dinheiro.java
+++ SmartWaiter/src/Dinheiro.java
@@ -1,9 +1,21 @@
 
 public class Dinheiro extends Pagamento {
  
+	static Dinheiro dinheiro;
+	
 	public void pagar() {
 	 
 	}
+
+	public static Pagamento obterInstancia(){
+		return dinheiro;
+	}
+	
+	private Dinheiro() {
+		Dinheiro.dinheiro = new Dinheiro();
+	}
+	
+	
 	 
 }
  
